8a818d8248200a7a0deab7c2ae6c3a7196564de3
gustavooviedo <gustavooviedo1421@gmail.com>
2024-10-18 17:18:43 -0400

    Landing page styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -8,22 +9,11 @@ import Photography from './pages/Photography';
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About Me</Link></li>
-          <li><Link to="/projects">Coding Projects</Link></li>
-          <li><Link to="/photography">Photography</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/photography" element={<Photography />} />
-      </Routes>
-    </Router>
+    //<Router>
+      <div className="App">
+        <Home />
+      </div>
+    //</Router>
   );
 }
 
